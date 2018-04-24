be7f2552e16eef60b1bae0a7238ec95755f2954e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-04-24 15:51:27 +0200

    Add sorting function

--- src/main/scala/nl/biopet/utils/SemanticVersion.scala
+++ src/main/scala/nl/biopet/utils/SemanticVersion.scala
@@ -21,6 +21,7 @@
 
 package nl.biopet.utils
 import scala.util.matching.Regex
+import scala.math.pow
 
 /**
   * Created by pjvanthof on 29/04/2017.
@@ -28,7 +29,57 @@ import scala.util.matching.Regex
 case class SemanticVersion(major: Int,
                            minor: Int,
                            patch: Int,
-                           build: Option[String] = None) {
+                           build: Option[String] = None)
+    extends Ordering[SemanticVersion] {
+  /**
+    * Converts the build to an Int for comparison purposes
+    * @return an integer
+    */
+  def buildToInt(): Int = {
+    // TODO: Improve algorithm for buildToInt
+    build match {
+      case Some(string) =>
+        // Currently only sorting on the first character of the build.
+        string.toCharArray.headOption match {
+          // The values are arbitrary. I have range -999 to 999 available for comparing the build.
+          // If a build is attached it should rank lower than the release. Therefore penalize.
+          // 100-char.asDigit. If build starts with "b" (11) this number will be higher than
+          // if it starts with a (10). Beta is higher than alpha.
+          case Some(char) =>
+            if (char.asDigit < 0) -150 else -(100 - char.asDigit)
+          // If empty => 0. If no build is attached this is the release.
+          case _ => 0
+        }
+      case _ => 0
+    }
+  }
+
+  /**
+    * Converts this version to an integer for comparison purposes
+    * Does not work properly with major, minor, or patch versions > 100
+    * @return the build to int.
+    */
+  def toInt(): Int = {
+    // Int.MaxValue == 2147483647
+    require(major < 214, "With a major value higher than 213 this class cannot be sorted.")
+    require(minor < 100, "With a minor value higher than 99 this class cannot be sorted.")
+    require(patch < 100, "With a patch value higher than 99 this class cannot be sorted.")
+    this.major * pow(10, 7) +
+      this.minor * pow(10, 5) +
+      this.patch * pow(10, 3) +
+      buildToInt()
+  }.toInt
+
+  def compare(x: SemanticVersion, y: SemanticVersion): Int = {
+    x.toInt() - y.toInt()
+  }
+
+  def ==(that: SemanticVersion): Boolean = {
+    this.major == that.major &&
+    this.minor == that.minor &&
+    this.patch == that.patch &&
+    this.build == that.build
+  }
   def >(that: SemanticVersion): Boolean = {
     if (this.major != that.major) this.major > that.major
     else if (this.minor != that.minor) this.minor > that.minor
